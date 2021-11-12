c56f496e65acb46f6dad9e2dd684ab872d44fc60
unknown <hoyzington@gmail.com>
2021-11-12 09:26:03 -0600

    implement

---
+++ src/main/java/com/revature/scottbank/util/ScreenRouter.java
@@ -0,0 +1,26 @@
+package com.revature.scottbank.util;
+
+import com.revature.scottbank.screens.Screen;
+
+public class ScreenRouter {
+
+    private final LinkedList<Screen> screens;
+
+    public ScreenRouter() {
+        screens = new LinkedList<>();
+    }
+
+    public void addScreen(Screen screen) {
+        screens.add(screen);
+    }
+
+    public void navigate(String route) throws Exception {
+        for (int i = 0; i < screens.size(); i++) {
+            Screen thisScreen = screens.get(i);
+            if (thisScreen.getRoute().equals(route)) {
+                thisScreen.render();
+            }
+        }
+    }
+
+}
