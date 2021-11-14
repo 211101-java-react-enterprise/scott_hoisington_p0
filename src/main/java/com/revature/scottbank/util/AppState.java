d0ed005cf3f233fd706bf732f62df14ae8b9bf1a
unknown <hoyzington@gmail.com>
2021-11-13 18:06:48 -0600

    add BufferedReader & ScreenRouter vars & methods

--- src/main/java/com/revature/scottbank/util/AppState.java
+++ src/main/java/com/revature/scottbank/util/AppState.java
@@ -1,17 +1,32 @@
 package com.revature.scottbank.util;
 
+import com.revature.scottbank.screens.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class AppState {
 
     private static boolean appRunning;
+    private final ScreenRouter router;
 
     public AppState() {
         appRunning = true;
+        router = new ScreenRouter();
+        BufferedReader consoleReader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        router.addScreen(new WelcomeScreen(consoleReader, router));
+        router.addScreen(new LoginScreen(consoleReader, router));
+        router.addScreen(new NewAcctScreen(consoleReader, router));
+        router.addScreen(new DashboardScreen(consoleReader, router));
+
     }
 
     public void startup() {
         try {
             while (appRunning) {
-
+                router.navigate("/welcome");
             }
         } catch (Exception e) {
             e.printStackTrace();
