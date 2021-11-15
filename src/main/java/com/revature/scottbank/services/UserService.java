820be789f15da62dc452d4cf933e973908a96dcd
unknown <hoyzington@gmail.com>
2021-11-14 19:31:07 -0600

    initial implementation

--- src/main/java/com/revature/scottbank/services/UserService.java
+++ src/main/java/com/revature/scottbank/services/UserService.java
@@ -1,5 +1,7 @@
 package com.revature.scottbank.services;
 
+import com.revature.scottbank.exceptions.AuthenticationException;
+import com.revature.scottbank.exceptions.InvalidRequestException;
 import com.revature.scottbank.models.AppUser;
 
 public class UserService {
@@ -7,17 +9,29 @@ public class UserService {
     private AppUser sessionUser;
 
     public UserService() {
-
+        this.sessionUser = null;
     }
 
     public AppUser getSessionUser() { return sessionUser; }
 
     public boolean registerNewUser(AppUser newUser) {
+        if (!isUserValid(newUser)) {
+            throw new InvalidRequestException("Please complete all information");
+        }
         return true;
     }
 
-    public void authUser() {
-
+    public void authUser(String email, String password) {
+        if (email == null || email.trim().equals("") || password == null ||
+                password.trim().equals("")) {
+            throw new InvalidRequestException("Email and password are required for logging " +
+                    "in");
+        }
+        AppUser authUser;
+        if (authUser == null) {
+            throw new AuthenticationException();
+        }
+        sessionUser = authUser;
     }
 
     public void logout() { sessionUser = null; }
