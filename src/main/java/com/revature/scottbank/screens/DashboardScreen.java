936a3a8ff00fb6330ef7bca61a231ad41e18c1fa
unknown <hoyzington@gmail.com>
2021-11-17 13:34:23 -0600

    debug logout

--- src/main/java/com/revature/scottbank/screens/DashboardScreen.java
+++ src/main/java/com/revature/scottbank/screens/DashboardScreen.java
@@ -8,8 +8,6 @@ import com.revature.scottbank.util.ScreenRouter;
 
 import java.io.BufferedReader;
 
-import static com.revature.scottbank.util.AppState.shutdown;
-
 public class DashboardScreen extends Screen {
 
     private final UserService userService;
@@ -25,33 +23,35 @@ public class DashboardScreen extends Screen {
     @Override
     public void render() throws Exception {
         AppUser sessionUser = userService.getSessionUser();
-        Account sessionAcct = acctService.findMyAcct();
-        System.out.printf("\n%s %s\n", sessionUser.getFirstName(),
-                sessionUser.getLastName());
-        System.out.printf("Account %s\n", sessionAcct.getId());
-        double balance = (Math.round(sessionAcct.getBalance() * 100)) / 100.0f;
-        System.out.printf("Balance: $%,8.2f\n", balance);
-        String menu = "1) Deposit to Your Account\n" +
-                "2) Withdraw from Your Account\n" +
-                "3) Log Out\n" +
-                "> ";
-        System.out.print(menu);
-        String userSelection = consoleReader.readLine();
+        while (userService.isSessionActive()) {
+            Account sessionAcct = acctService.findMyAcct();
+            System.out.printf("\n%s %s\n", sessionUser.getFirstName(),
+                    sessionUser.getLastName());
+            System.out.printf("Account %s\n", sessionAcct.getId());
+            double balance = (Math.round(sessionAcct.getBalance() * 100)) / 100.0f;
+            System.out.printf("Balance: $%,8.2f\n", balance);
+            String menu = "1) Deposit to Your Account\n" +
+                    "2) Withdraw from Your Account\n" +
+                    "3) Log Out\n" +
+                    "> ";
+            System.out.print(menu);
+            String userSelection = consoleReader.readLine();
 
-        switch (userSelection) {
-            case "1":
-                router.navigate("/deposit");
-                break;
-            case "2":
-                router.navigate("/withdraw");
-                break;
-            case "3":
-                System.out.println("\nGoodbye. Come back soon.\n\n");
-                shutdown();
-                break;
-            default:
-                System.out.println("\nInvalid Selection. Please try again.\n");
-                router.navigate("/dashboard");
+            switch (userSelection) {
+                case "1":
+                    router.navigate("/deposit");
+                    break;
+                case "2":
+                    router.navigate("/withdraw");
+                    break;
+                case "3":
+                    System.out.println("\nGoodbye. Come back soon.\n\n");
+                    userService.logout();
+                    break;
+                default:
+                    System.out.println("\nInvalid Selection. Please try again.\n");
+                    router.navigate("/dashboard");
+            }
         }
     }
 
