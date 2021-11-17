0fa3ab618edae9031f29d9fd95c25841eda87456
unknown <hoyzington@gmail.com>
2021-11-16 23:18:07 -0600

    add logger to screens

--- src/main/java/com/revature/scottbank/screens/Screen.java
+++ src/main/java/com/revature/scottbank/screens/Screen.java
@@ -1,5 +1,6 @@
 package com.revature.scottbank.screens;
 
+import com.revature.scottbank.util.Logger;
 import com.revature.scottbank.util.ScreenRouter;
 
 import java.io.BufferedReader;
@@ -9,12 +10,14 @@ public abstract class Screen {
     String route;
     BufferedReader consoleReader;
     ScreenRouter router;
+    Logger logger;
 
     public Screen(String route, BufferedReader consoleReader,
                   ScreenRouter router) {
         this.route = route;
         this.consoleReader = consoleReader;
         this.router = router;
+        logger = Logger.getLogger(true);
     }
 
     public String getRoute() {
