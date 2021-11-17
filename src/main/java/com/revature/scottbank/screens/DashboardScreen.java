95df9ba47edbd3447202badb9f463c1f171e3f87
unknown <hoyzington@gmail.com>
2021-11-16 23:22:53 -0600

    finish implementation

--- src/main/java/com/revature/scottbank/screens/DashboardScreen.java
+++ src/main/java/com/revature/scottbank/screens/DashboardScreen.java
@@ -1,5 +1,8 @@
 package com.revature.scottbank.screens;
 
+import com.revature.scottbank.models.Account;
+import com.revature.scottbank.models.AppUser;
+import com.revature.scottbank.services.AcctService;
 import com.revature.scottbank.services.UserService;
 import com.revature.scottbank.util.ScreenRouter;
 
@@ -10,18 +13,24 @@ import static com.revature.scottbank.util.AppState.shutdown;
 public class DashboardScreen extends Screen {
 
     private final UserService userService;
+    private final AcctService acctService;
 
     public DashboardScreen(BufferedReader consoleReader, ScreenRouter router,
-                           UserService userService) {
+                           UserService userService, AcctService acctService) {
         super("/dashboard", consoleReader, router);
         this.userService = userService;
+        this.acctService = acctService;
     }
 
     @Override
     public void render() throws Exception {
-        System.out.printf("\n%s %s\n", "User", "Userton");
-        System.out.printf("Account %s\n", "09245892039");
-        System.out.printf("Balance: $%s\n", "100.00");
+        AppUser sessionUser = userService.getSessionUser();
+        Account sessionAcct = acctService.findMyAcct();
+        System.out.printf("\n%s %s\n", sessionUser.getFirstName(),
+                sessionUser.getLastName());
+        System.out.printf("Account %s\n", sessionAcct.getId());
+        double balance = (Math.round(sessionAcct.getBalance() * 100)) / 100.0f;
+        System.out.printf("Balance: $%,8.2f\n", balance);
         String menu = "1) Deposit to Your Account\n" +
                 "2) Withdraw from Your Account\n" +
                 "3) Log Out\n" +
@@ -42,6 +51,7 @@ public class DashboardScreen extends Screen {
                 break;
             default:
                 System.out.println("\nInvalid Selection. Please try again.\n");
+                router.navigate("/dashboard");
         }
     }
 
